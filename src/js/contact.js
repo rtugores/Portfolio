1d467837e536b9079de8f06e6a90957fe6caf52a
Mingos <dcividanes91@gmail.com>
2015-04-27 19:08:41 +0200

    xsmall css adapted

--- src/js/contact.js
+++ src/js/contact.js
@@ -1,27 +1,30 @@
 function sendMail(){
 	this.event.preventDefault();
-  $.ajax({
-    type: "POST",
-    url: "https://mandrillapp.com/api/1.0/messages/send.json",
-    data: {
-      'key': 'SfwRClgjlAwZ4dKqOQRuog',
-      'message': {
-        'from_email': $("#email").val(),
-        'from_name': $("#name").val(),
-        'subject': 'Portafolio Contact',
-        'html': $("#message").val(),
-        'to': [
-          {
-            'email': 'dcividanes91@gmail.com',
-            'name': 'Domingo Cividanes',
-            'type': 'to'
-          }
-        ],
+  if( $("#email").val() && $("#name").val() && $("#message").val() ){
+    $.ajax({
+      type: "POST",
+      url: "https://mandrillapp.com/api/1.0/messages/send.json",
+      data: {
+        'key': 'SfwRClgjlAwZ4dKqOQRuog',
+        'message': {
+          'from_email': $("#email").val(),
+          'from_name': $("#name").val(),
+          'subject': 'Portafolio Contact',
+          'html': $("#message").val(),
+          'to': [
+            {
+              'email': 'dcividanes91@gmail.com',
+              'name': 'Domingo Cividanes',
+              'type': 'to'
+            }
+          ],
+        }
       }
-    }
-  }).done(function(response) {
-  	alert("Message sent successfully");
-	}).fail(function(response){
-		alert("Message can't be sent");
-	});
+    }).done(function(response) {
+    	alert("Message sent successfully");
+  	}).fail(function(response){
+  		alert("Message can't be sent");
+  	}); 
+  }
+  alert("Fill all fields before sending");
 }
--- src/js/init_app.js
+++ src/js/init_app.js
@@ -119,28 +119,31 @@ var skel=function(){var e="breakpoints",t="config",n="iterate",r="elements",i="s
 
 })(jQuery);;function sendMail(){
 	this.event.preventDefault();
-  $.ajax({
-    type: "POST",
-    url: "https://mandrillapp.com/api/1.0/messages/send.json",
-    data: {
-      'key': 'SfwRClgjlAwZ4dKqOQRuog',
-      'message': {
-        'from_email': $("#email").val(),
-        'from_name': $("#name").val(),
-        'subject': 'Portafolio Contact',
-        'html': $("#message").val(),
-        'to': [
-          {
-            'email': 'dcividanes91@gmail.com',
-            'name': 'Domingo Cividanes',
-            'type': 'to'
-          }
-        ],
+  if( $("#email").val() && $("#name").val() && $("#message").val() ){
+    $.ajax({
+      type: "POST",
+      url: "https://mandrillapp.com/api/1.0/messages/send.json",
+      data: {
+        'key': 'SfwRClgjlAwZ4dKqOQRuog',
+        'message': {
+          'from_email': $("#email").val(),
+          'from_name': $("#name").val(),
+          'subject': 'Portafolio Contact',
+          'html': $("#message").val(),
+          'to': [
+            {
+              'email': 'dcividanes91@gmail.com',
+              'name': 'Domingo Cividanes',
+              'type': 'to'
+            }
+          ],
+        }
       }
-    }
-  }).done(function(response) {
-  	alert("Message sent successfully");
-	}).fail(function(response){
-		alert("Message can't be sent");
-	});
+    }).done(function(response) {
+    	alert("Message sent successfully");
+  	}).fail(function(response){
+  		alert("Message can't be sent");
+  	}); 
+  }
+  alert("Fill all fields before sending");
 }
